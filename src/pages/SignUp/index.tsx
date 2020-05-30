b8b05a0e403d9b5665864d700e9cc941016a044f
João Nascimento <joao1991algoritmo@gmail.com>
2020-05-30 13:13:21 -0300

    Configuração do unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 // React imports
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 // Assets imports
 import logoImg from '../../assets/logo.svg';
@@ -12,47 +13,53 @@ import Button from '../../components/button';
 // Styles Components imports
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <>
-    <Container>
-      <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  }
 
-      <Content>
-        <img src={logoImg} alt="GoBarber" />
+  return (
+    <>
+      <Container>
+        <Background />
+        <Content>
+          <img src={logoImg} alt="GoBarber" />
 
-        <form>
-          <h1>Faça seu cadastro</h1>
+          <Form initialData={{}} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-          <Input
-            icon={FiUser}
-            name="name"
-            type="text"
-            placeholder="Informe o nome"
-          />
+            <Input
+              icon={FiUser}
+              name="name"
+              type="text"
+              placeholder="Informe o nome"
+            />
 
-          <Input
-            icon={FiMail}
-            name="email"
-            type="text"
-            placeholder="Informar o E-mail"
-          />
+            <Input
+              icon={FiMail}
+              name="email"
+              type="text"
+              placeholder="Informar o E-mail"
+            />
 
-          <Input
-            icon={FiLock}
-            name="password"
-            type="password"
-            placeholder="Informar a senha"
-          />
+            <Input
+              icon={FiLock}
+              name="password"
+              type="password"
+              placeholder="Informar a senha"
+            />
 
-          <Button type="submit">Cadastrar</Button>
-        </form>
+            <Button type="submit">Cadastrar</Button>
+          </Form>
 
-        <a href="CreateAccount">
-          <FiArrowLeft /> Voltar para o logon
-        </a>
-      </Content>
-    </Container>
-  </>
-);
+          <a href="CreateAccount">
+            <FiArrowLeft /> Voltar para o logon
+          </a>
+        </Content>
+      </Container>
+    </>
+  );
+};
 
 export default SignUp;
