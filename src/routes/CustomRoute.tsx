8d9d1d78e581943270c9c054daed95ed78928e7d
João Nascimento <joao1991algoritmo@gmail.com>
2020-06-07 13:01:14 -0300

    Rotas autenticadas

---
+++ src/routes/CustomRoute.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route, RouteProps, Redirect } from 'react-router-dom';
+
+import { useAuth } from '../hooks/AuthContext';
+
+interface CustomRouteProps extends RouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+// true/true => ok
+// true/false => Redirecionar para o login
+// false/true => Redirecionar para o dashboard(Login e criação de conta)
+// false/false => ok
+const CustomRoute: React.FC<CustomRouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : 'dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default CustomRoute;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import CustomRoute from './CustomRoute';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Dashboard from '../pages/Dashboard';
+
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
+    <CustomRoute path="/" exact component={SignIn} />
+    <CustomRoute path="/signup" component={SignUp} />
+    <CustomRoute path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
